d151dc7e57db289ba11a716f898fd8110cbc4f31
Samuel Plumppu <greenheartmusic@gmail.com>
2019-12-22 03:11:28 +0100

    Initial landing page

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,107 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
+
 import Nav from '../components/nav'
 
 const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div>
+        <Head>
+            <title>Home</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-    <Nav />
+        <Nav />
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+        <div className="hero">
+            <h1 className="title">Hack For Future</h1>
+            <p className="description">
+                A community of hackers and creators, working together to raise
+                awareness about the climate crisis.
+            </p>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
+            <div className="row">
+                <Link href="/">
+                    <a className="card">
+                        <h3>Our Community &rarr;</h3>
+                        <p>
+                            Hack For Future is a international network working
+                            on projects that combine climate, technology and
+                            communication. We collaborate both on events and
+                            online. Welcome to join us!
+                        </p>
+                    </a>
+                </Link>
+                <Link href="/">
+                    <a className="card">
+                        <h3>Our hackathons &rarr;</h3>
+                        <p>
+                            We arrange regular hackathons with local partners
+                            around the world. Learn what makes these events
+                            special - and find one near you.
+                        </p>
+                    </a>
+                </Link>
+                <Link href="/">
+                    <a className="card">
+                        <h3>Organize a hackathon &rarr;</h3>
+                        <p>
+                            Want to bring the hackathon spirit to your local
+                            area? Read more about how we'll help you get started
+                            organizing your own event.
+                        </p>
+                    </a>
+                </Link>
+            </div>
+        </div>
+
+        <style jsx>{`
+            .hero {
+                width: 100%;
+                color: #333;
+            }
+            .title {
+                margin: 0;
+                width: 100%;
+                padding-top: 80px;
+                line-height: 1.15;
+                font-size: 48px;
+            }
+            .title,
+            .description {
+                text-align: center;
+            }
+            .row {
+                max-width: 880px;
+                margin: 80px auto 40px;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-around;
+            }
+            .card {
+                padding: 18px 18px 24px;
+                width: 220px;
+                text-align: left;
+                text-decoration: none;
+                color: #434343;
+                border: 1px solid #9b9b9b;
+            }
+            .card:hover {
+                border-color: #067df7;
+            }
+            .card h3 {
+                margin: 0;
+                color: #067df7;
+                font-size: 18px;
+            }
+            .card p {
+                margin: 0;
+                padding: 12px 0 0;
+                font-size: 13px;
+                color: #333;
+            }
+        `}</style>
     </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
 )
 
 export default Home
