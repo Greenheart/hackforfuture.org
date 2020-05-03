82fca335e455e867b66c121bcbfebc28028c477b
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-03 04:08:54 +0200

    fix(imports): fix module failed import and remove unused import in other component

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 const Card = ({ title = '', href = '', text = '' }) => (
     <Link href={href}>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from 'next/link'
 
 import Layout from '../components/Layout'
 import PageIntro from '../components/PageIntro'
