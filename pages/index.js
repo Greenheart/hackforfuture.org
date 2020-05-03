36fbe01a7429d10cbac34227dddb1514e9f14838
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-03 02:12:50 +0200

    feat(design): Add basic cards for the landing page

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,19 @@ import Link from 'next/link'
 
 import Layout from '../components/Layout'
 
+const Card = ({ title = '', href = '', text = '' }) => (
+    <Link href={href}>
+        <a className="bg-white rounded-lg p-4">
+            <h3>{title} &rarr;</h3>
+            <p>{text}</p>
+        </a>
+    </Link>
+)
+
 const Home = () => (
     <Layout>
         <div>
-            <div className="text-center text-white">
+            <div className="text-center text-white leading-relaxed">
                 <h1 className="font-black text-4xl xs:text-5xl sm:text-6xl">
                     Hack for Future
                 </h1>
@@ -17,38 +26,22 @@ const Home = () => (
                 <p>Join our next climate hackathon on May 9th!</p>
             </div>
 
-            <div className="">
-                <Link href="/community">
-                    <a className="">
-                        <h3>Our community &rarr;</h3>
-                        <p>
-                            Hack for Future is a international network working
-                            on projects that combine climate, technology and
-                            communication. We collaborate both on events and
-                            online. Welcome to join us!
-                        </p>
-                    </a>
-                </Link>
-                <Link href="/hackathons">
-                    <a className="">
-                        <h3>Our hackathons &rarr;</h3>
-                        <p>
-                            We arrange regular hackathons with local partners
-                            around the world. Learn what makes these events
-                            special - and find one near you.
-                        </p>
-                    </a>
-                </Link>
-                <Link href="/join">
-                    <a className="">
-                        <h3>Organize a hackathon &rarr;</h3>
-                        <p>
-                            Want to bring the hackathon spirit to your local
-                            area? Read more about how we'll help you get started
-                            organizing your own event.
-                        </p>
-                    </a>
-                </Link>
+            <div className="grid grid-cols-3 gap-8 mt-20 mb-20">
+                <Card
+                    title="Community"
+                    href="/community"
+                    text="Hack for Future is a international network working on projects that combine climate, technology and communication. We collaborate both on events and online. Welcome to join us!"
+                />
+                <Card
+                    title="Climate hackathons"
+                    href="/hackathons"
+                    text="We arrange regular hackathons with local partners around the world. Learn what makes these events special - and find one near you."
+                />
+                <Card
+                    title="Welcome to join us!"
+                    href="/join"
+                    text="Want to bring the hackathon spirit to your local area? Read more about how we'll help you get started organizing your own event."
+                />
             </div>
         </div>
     </Layout>
