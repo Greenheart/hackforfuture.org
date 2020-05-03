291b12ef7bf2a7454bbcfa5281e36338653e6381
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-03 04:25:27 +0200

    fix(keys): add keys for generated JSX elements

--- components/PageIntro.js
+++ components/PageIntro.js
@@ -4,8 +4,8 @@ const PageIntro = ({ title = '', text = [] }) => {
     return (
         <div className="text-center text-white text-lg sm:text-xl leading-relaxed">
             <h1 className="font-black text-5xl sm:text-6xl">{title}</h1>
-            {text.map((t) => (
-                <p>{t}</p>
+            {text.map((t, i) => (
+                <p key={t + i}>{t}</p>
             ))}
         </div>
     )
