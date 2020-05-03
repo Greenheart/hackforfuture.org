84a3f5a8aa63dbb26390b19339d81dfea335e0fd
Samuel Plumppu <greenheartmusic@gmail.com>
2020-05-03 03:39:48 +0200

    fix(design): make footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Footer = ({ className = '' }) => {
     return (
-        <footer className={`flex justify-center p-4 text-white ${className}`}>
-            <div>
-                <span className="mr-10">
+        <footer className={`flex justify-center p-4 ${className}`}>
+            <div className="flex flex-wrap justify-evenly w-screen">
+                <span className="px-8">
                     &copy; {new Date().getFullYear()} Hack for Future
                 </span>
-                <a href="mailto:hackforfuture@gmail.com">
+                <a href="mailto:hackforfuture@gmail.com" className="px-8">
                     hackforfuture@gmail.com
                 </a>
             </div>
