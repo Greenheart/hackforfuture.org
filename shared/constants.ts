d08ec683c96075bcf26cfea00cbf27236907d703
Samuel Plumppu <greenheartmusic@gmail.com>
2021-01-29 01:11:44 +0100

    Update community invite

--- shared/constants.ts
+++ shared/constants.ts
@@ -2,5 +2,17 @@ export const email = 'hackforfuture@gmail.com'
 export const siteName = 'Hack for Future'
 export const description =
     'A creative community of entrepreneurs, designers and developers working on open source solutions for a sustainable future'
-export const communityInvite =
-    'https://matrix.to/#/!RfWpjFNKGUIYotoPZZ:matrix.org?via=matrix.org'
+
+const subject = `I want to join! 🌱`
+const body = `Hi! My name is 
+
+I'd like to join because... 
+I want to learn... 
+I want to contribute with... 
+I learned about ${siteName} through... 
+
+See you soon!`
+
+export const communityInvite = `mailto:${email}?subject=${encodeURIComponent(
+    subject,
+)}&body=${encodeURIComponent(body)}`
