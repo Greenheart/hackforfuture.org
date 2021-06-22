4f0993a4b7bf816057f6d48af9afebd1f6599073
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-06-22 20:08:53 +0200

    WIP: about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,19 @@
-import { Heading, Link, Text, Center } from '@chakra-ui/react'
+import { Heading, Link, Text, Center, Box } from '@chakra-ui/react'
 import NextLink from 'next/link'
+import Image from 'next/image'
 
 import Layout from '../layouts/Layout'
 import PrimaryCTA from '../components/PrimaryCTA'
 import Partners from '../components/Partners'
-import Image from 'next/image'
 
-import { siteName } from '../shared/constants'
+import { siteName, description } from '../shared/constants'
 import Divider from '../components/Divider'
 
 const About = () => (
     <Layout maxW="xl" fontSize={[16, null, 20]}>
-        <Heading>About us</Heading>
+        <Heading textAlign="center" mb="8">
+            About us
+        </Heading>
         <Center mb="8">
             <Image
                 src="/images/undraw_real_time_collaboration_c62i.svg"
@@ -22,9 +24,34 @@ const About = () => (
                 height={'auto'}
             />
         </Center>
-        <Text mt="2">The history behind why {siteName} exists.</Text>
 
-        <Text mt="4">And another paragraph.</Text>
+        <Box fontSize={['md', null, 'lg']} fontWeight="normal">
+            <Text mb="4">
+                Founded in October, 2019, {siteName} is a {description.slice(1)}
+                .
+            </Text>
+            <Text mb="4">
+                We initially organized 7 climate hackathons between October 2019
+                - June 2020, but figured we wanted to focus more on building
+                interesting projects rather than organizing events. That's why
+                we now meet for weekly community calls to make regular progress
+                on our projects and have fun together.
+            </Text>
+            <Text>
+                It's a great way to learn learn new things and build your
+                network within the impact tech industry. Join us!
+            </Text>
+        </Box>
+
+        {/* IDEA: Add projects that have been created in the community */}
+        {/* <Projects /> */}
+        {/*
+            Swipe for Future
+            Treehugger
+            FutureVisions.earth
+            Rating Municipalities for Aktuell Hållbarhet
+        */}
+
         <Center>
             <PrimaryCTA />
         </Center>
