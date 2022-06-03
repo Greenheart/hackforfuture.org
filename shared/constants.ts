26f48f2161bf7a26095625caba77993e347ffdf6
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-06-03 17:17:51 +0200

    Cleanup and fixes

--- shared/constants.ts
+++ shared/constants.ts
@@ -3,16 +3,5 @@ export const siteName = 'Hack for Future'
 export const description =
     'A creative community of entrepreneurs, developers and designers working on open source solutions for a sustainable future'
 
-const subject = `I want to join! 🌱`
-const body = `Hi! My name is 
-
-I'd like to join because... 
-I want to learn... 
-I want to contribute with... 
-I learned about ${siteName} through... 
-
-See you soon!`
-
-export const communityInvite = `mailto:${email}?subject=${encodeURIComponent(
-    subject,
-)}&body=${encodeURIComponent(body)}`
+export const communityInvite =
+    'https://app.element.io/#/room/#hackforfuture:matrix.org'
